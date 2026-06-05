389523f70c0696af5110b6a0adfb4af01da3574b
adelinaelsandra-gif <adelinaelsandra@gmail.com>
2026-06-05 15:32:22 +0700

    ADD ABSTRACT CLASS ORANG

--- Asosiasi.java
+++ Asosiasi.java
@@ -1,3 +1,15 @@
 import java.util.ArrayList;
-    
+    abstract class Orang {
+    private String nama;
+
+    public Orang(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public abstract void tampilkanPeran();
 }
+
